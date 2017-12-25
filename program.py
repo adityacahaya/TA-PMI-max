c05edd3ccfdf92df5eaf2e953c2e5a6f77321b5a
adityacahaya <kadekadityacahaya@gmail.com>
2017-12-25 21:28:18 +0700

    reinitialized files

--- program.py
+++ program.py
@@ -1,5 +1,6 @@
 import MySQLdb
 import numpy
+numpy.set_printoptions(threshold=numpy.nan)
 
 # konek ke db
 db = MySQLdb.connect(host="localhost",port=3306,user="root",passwd="",db = "tugasakhir")
@@ -16,7 +17,7 @@ cursor = db.cursor()
 #    print(row)
 
 # baca file corpus
-fileCorpus = open("katates.txt", "r") 
+fileCorpus = open("corpus_tes.txt", "r") 
 # insert kata-kata corpus ke dalam list
 stringList = fileCorpus.read().strip().split(" ")
 # hilangkan null/"" dari list
@@ -27,13 +28,18 @@ stringList = list(set(stringList))
 stringList.sort()
 
 # print list kata unik
-print("List Kata Corpus Unik : ")
-print(stringList)
-print("\n") 
+#print("List Kata Corpus Unik : ")
+#print(stringList)
+#print("\n") 
+
 # print jumlah kata unik
 totalKataUnik = len(stringList)
-print("Jumlah Kata Unik : ",totalKataUnik)
-print("\n")
+s = (totalKataUnik + 2, totalKataUnik + 2)
+#print("Jumlah Kata Unik : ",totalKataUnik)
+#print("\n")
 
-matrix = numpy.zeros((5, 5))
-print(matrix)
+matrixAkhir = numpy.zeros(s, dtype = object)
+for x in range(totalKataUnik) :
+    matrixAkhir[0][x+1] = stringList[x]
+    matrixAkhir[x+1][0] = stringList[x]
+numpy.savetxt('matrixAkhir.csv', matrixAkhir, delimiter=',', fmt='%s')
